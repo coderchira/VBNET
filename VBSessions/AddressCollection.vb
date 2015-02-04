9817ccd8f72d9c8998c3a2028bb44a9ce9c113db
coderchira <ruchira.aggarwal@gmail.com>
2015-02-04 16:11:25 +0530

    Problem 2 - AddressCollection

---
+++ VBSessions/AddressCollection.vb
@@ -0,0 +1,26 @@
+﻿Module AddressCollection
+    Sub MySub()
+        Dim CNames As Collection
+        CNames = New Collection
+
+        CNames.Add("ruchira")
+        CNames.Add("ruchi")
+
+        Dim CEmails As Collection
+        CEmails = New Collection
+
+        CEmails.Add("ruchira_agarwal")
+        CEmails.Add("ruchi_abc")
+
+        Dim CAddr As Collection
+        CAddr = New Collection
+
+        'For Each CItem As Host in CNames,CEmails
+        '    CItem.Add(CNames.IndexOf(CNames) + "," + CEmails.IndexOf(CEmails))
+        'Next
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
