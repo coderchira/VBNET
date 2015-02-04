beafc6c712aac1218eb33d8b189c8147c961c163
coderchira <ruchira.aggarwal@gmail.com>
2015-02-04 16:22:57 +0530

    Problem 2 - AddressCollection

--- VBSessions/AddressCollection.vb
+++ VBSessions/AddressCollection.vb
@@ -1,26 +1,23 @@
 ﻿Module AddressCollection
-    Sub MySub()
-        Dim CNames As Collection
-        CNames = New Collection
+    '3 collections:
+    'collection 1  has names
+    'Collection 2 has emails 
+    'Code such that collection 3 is going to be a collection of collection representing a complete address
 
+    Sub MySub()
+        Dim CNames As New Collection
         CNames.Add("ruchira")
         CNames.Add("ruchi")
 
-        Dim CEmails As Collection
-        CEmails = New Collection
-
+        Dim CEmails As New Collection
         CEmails.Add("ruchira_agarwal")
         CEmails.Add("ruchi_abc")
 
-        Dim CAddr As Collection
-        CAddr = New Collection
+        Dim CAddr As New Collection
 
         'For Each CItem As Host in CNames,CEmails
         '    CItem.Add(CNames.IndexOf(CNames) + "," + CEmails.IndexOf(CEmails))
         'Next
-
         Console.ReadLine()
-
     End Sub
-
 End Module
